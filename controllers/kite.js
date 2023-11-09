b5eacad91deb0d5a435ebf9c4e9d7e908fc65241
Naveenreddy70 <142835943+Naveenreddy70@users.noreply.github.com>
2023-11-09 13:44:59 -0600

    files

--- controllers/kite.js
+++ controllers/kite.js
@@ -38,3 +38,23 @@ exports.kite_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+    // Handle kite create on POST.
+exports.kite_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new kite();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"kite_type":"goat", "cost":12, "size":"large"}
+    document.Kite_name = req.body.Kite_name;
+    document.kite_color = req.body.kite_color;
+    document.kite_price = req.body.kite_price;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
