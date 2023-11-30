3eee3e556a1834d22f8050ca83b6ee25f6cb4fa2
Naveenreddy70 <142835943+Naveenreddy70@users.noreply.github.com>
2023-11-29 22:07:42 -0600

    Update kite.js

--- models/kite.js
+++ models/kite.js
@@ -1,7 +1,9 @@
+
+
 const mongoose = require("mongoose")
 const kiteSchema = mongoose.Schema({
- kite_color: String,    
-kite_name: String,
-kite_price: Number
+    kite_color: String, 
+    kite_name:  {type: String,maxLength: 15},
+    kite_price: {type: Number,min: 0,max: 200}
 })
-module.exports = mongoose.model("kite", kiteSchema);
+module.exports = mongoose.model("kite",kiteSchema)
